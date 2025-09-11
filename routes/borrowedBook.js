9a407776d5675ce19bbe62260a3334b360ba4c54
Dev99999999 <sonawanedevendra109@gmail.com>
2025-09-11 11:44:09 +0530

    nodejs project is ready..

--- routes/borrowedBook.js
+++ routes/borrowedBook.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const { borrowed, getBorrowedBook, totalFine, alldata } = require("../controllers/borrowedBook.js")
+const { authMiddleware, authorizeRoles } = require("../middleware/auth.js")
 
 router.post("/borrowedbook", borrowed)
-router.get("/information", getBorrowedBook)
-router.get("/totalfine",totalFine)
-router.get("/allData", alldata)
+router.get("/information", authMiddleware, authorizeRoles("admin"), getBorrowedBook)
+router.get("/totalfine",authMiddleware, authorizeRoles("admin"), totalFine)
+router.get("/allData",authMiddleware, authorizeRoles("admin"), alldata)
 
 module.exports = router
 
