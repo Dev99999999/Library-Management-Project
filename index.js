4ac1833bc405eefada86e3cda85b6a4142fb0252
Dev99999999 <sonawanedevendra109@gmail.com>
2025-09-12 11:32:06 +0530

    nodejs project is ready..

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const connectDatabase = require("./db.js")
 const User = require("./routes/user.js")
 const Book = require("./routes/book.js")
 const borrowedBook = require("./routes/borrowedBook.js")
+const path = require("path")
+const expressLayouts = require("express-ejs-layouts");
 
 connectDatabase()
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -15,6 +24,10 @@ app.use(express.urlencoded({extended: true}))
 app.use("/api",User)
 app.use("/api",Book)
 app.use("/api",borrowedBook)
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Library Management" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
