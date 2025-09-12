55c23f512a7da23a0f9b3ee5f0787b91c00c26a4
Dev99999999 <sonawanedevendra109@gmail.com>
2025-09-12 11:37:11 +0530

    nodejs project is ready..

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const connectDatabase = require("./db.js")
 const User = require("./routes/user.js")
 const Book = require("./routes/book.js")
 const borrowedBook = require("./routes/borrowedBook.js")
-const path = require("path")
-const expressLayouts = require("express-ejs-layouts");
+// const path = require("path")
+// const expressLayouts = require("express-ejs-layouts");
 
 connectDatabase()
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
 
-app.use(expressLayouts);
-app.set("layout", "layouts/layout");
+// app.use(expressLayouts);
+// app.set("layout", "layouts/layout");
 
 
 app.use(express.json())
