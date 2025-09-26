763ce45889b5bc556f5b117895781f7ef7cc78df
Dev99999999 <sonawanedevendra109@gmail.com>
2025-09-26 12:15:33 +0530

    famous book and most active user..find completed..

--- reports/report.js
+++ reports/report.js
@@ -1,14 +1,42 @@
-const borrowedBook = require("../models/borrowedBook.js")
 const borrowedBookModel = require("../models/borrowedBook.js")
-const user = require("../models/user.js")
 
 const getMostBookReader = async (req, res) => {
     try {
         const result = await borrowedBookModel.aggregate([
             {
                 $group: {
+                    _id: { userId: "$userId", bookId: "$bookId" },
+                    borrowCount: { $sum: 1 } 
+                }
+            },
+            {
+                $lookup: {
+                    from: "books",
+                    localField: "_id.bookId",
+                    foreignField: "_id",
+                    as: "bookDetails"
+                }
+            },
+            { 
+                $unwind: "$bookDetails" 
+            },
+
+            {
+                $project: {
+                    _id: 0,
+                    userId: "$_id.userId",
+                    book: {
+                        book_name:"$bookDetails.title",
+                        borrowCount: "$borrowCount"
+                    }
+                }
+            },
+
+            {
+                $group: {
                     _id: "$userId",
-                    totalborrowbook: { $sum: 1 }
+                    totalborrowbook: { $sum: "$book.borrowCount"},
+                    books: { $push: "$book" }
                 }
             },
             { 
@@ -30,12 +58,14 @@ const getMostBookReader = async (req, res) => {
             { 
                 $unwind: "$userDetails" 
             },
+            
             {
                 $project: {
                     _id: 0,
                     user_name: "$userDetails.name",
                     email: "$userDetails.email",
-                    totalborrowbook: 1
+                    totalborrowbook: 1,
+                    books: 1
                 }
             }
         ])
@@ -53,18 +83,164 @@ const getMostBookReader = async (req, res) => {
     }
 }
 
+// const getMostBookReader = async (req, res) => {
+//     try {
+//         const result = await borrowedBookModel.aggregate([
+//             {
+//                 $group: {
+//                     _id: { userId: "$userId", bookId: "$bookId" },
+//                     borrowCount: { $sum: 1 }
+//                 }
+//             },
+//             {
+//                 $lookup: {
+//                     from: "books",
+//                     localField: "_id.bookId",
+//                     foreignField: "_id",
+//                     as: "bookDetails"
+//                 }
+//             },
+//             { $unwind: "$bookDetails" },
+//             {
+//                 $project: {
+//                     _id: 0,
+//                     userId: "$_id.userId",
+//                     book: {
+//                         title: "$bookDetails.title",
+//                         borrowCount: "$borrowCount"
+//                     }
+//                 }
+//             },
+//             {
+//                 $group: {
+//                     _id: "$userId",
+//                     totalborrowbook: { $sum: "$book.borrowCount" },
+//                     books: { $push: "$book" }
+//                 }
+//             },
+//             { $sort: { totalborrowbook: -1 } },
+//             { $limit: 3 },
+//             {
+//                 $lookup: {
+//                     from: "users",
+//                     localField: "_id",
+//                     foreignField: "_id",
+//                     as: "userDetails"
+//                 }
+//             },
+//             { $unwind: "$userDetails" },
+//             {
+//                 $project: {
+//                     _id: 0,
+//                     user_name: "$userDetails.name",
+//                     email: "$userDetails.email",
+//                     totalborrowbook: 1,
+//                     books: 1
+//                 }
+//             }
+//         ]);
+
+//         res.status(200).json({ success: true, data: result });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(400).json({ success: false, message: "Something went wrong here.." });
+//     }
+// }
+
+
+// const mostfamousBook = async (req, res) => {
+//     try {
+//         const result = await borrowedBookModel.aggregate([
+//             {
+//                 $group: {
+//                     _id: "$bookId",
+//                     userMostborrow: { $sum: 1 }
+//                 }
+//             },
+//             { 
+//                 $sort: { 
+//                     userMostborrow: -1
+//                 } 
+//             },
+//             { 
+//                 $limit:  3
+//             },
+//             {
+//                 $lookup: {
+//                     from: "books",
+//                     localField: "_id",
+//                     foreignField: "_id",
+//                     as: "bookDetails"
+//                 }
+//             },
+//             { 
+//                 $unwind: "$bookDetails" 
+//             },
+//             {
+//                 $project: {
+//                     _id: 0,
+//                     book_name: "$bookDetails.title",
+//                     userMostborrow: 1
+//                 }
+//             }
+//         ])
+
+//         res.status(200).json({
+//             success: true,
+//             data: result
+//         })
+//     } catch (error) {
+//         console.log(error)
+//         res.status(400).json({
+//             success: false,
+//             message: "Something went wrong here.."
+//         })
+//     }
+// }
+
 const mostfamousBook = async (req, res) => {
     try {
         const result = await borrowedBookModel.aggregate([
             {
                 $group: {
+                    _id: { userId: "$userId", bookId: "$bookId" },
+                    borrowCount: { $sum: 1 } 
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "_id.userId",
+                    foreignField: "_id",
+                    as: "userDetails"
+                }
+            },
+            { 
+                $unwind: "$userDetails" 
+            },
+
+            {
+                $project: {
+                    _id: 0,
+                    bookId: "$_id.bookId",
+                    user: {
+                        user_name:"$userDetails.name",
+                        // user_email: "$userDetails.email",
+                        borrowCount: "$borrowCount"
+                    }
+                }
+            },
+
+            {
+                $group: {
                     _id: "$bookId",
-                    userMostborrow: { $sum: 1 }
+                    totalborrowbook: { $sum: "$user.borrowCount"},
+                    users: { $push: "$user"}
                 }
             },
             { 
                 $sort: { 
-                    userMostborrow: -1
+                    totalborrowbook: -1
                 } 
             },
             { 
@@ -75,17 +251,19 @@ const mostfamousBook = async (req, res) => {
                     from: "books",
                     localField: "_id",
                     foreignField: "_id",
-                    as: "bookDetails"
+                    as: "bookDatails"
                 }
             },
             { 
-                $unwind: "$bookDetails" 
+                $unwind: "$bookDatails" 
             },
+            
             {
                 $project: {
                     _id: 0,
-                    book_name: "$bookDetails.title",
-                    userMostborrow: 1
+                    book_name: "$bookDatails.title",
+                    totalborrowbook: 1,
+                    users: 1
                 }
             }
         ])
